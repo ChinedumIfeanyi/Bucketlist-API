1c18e88f10d115fb7efe12a172d71572094c681e
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-03-07 23:54:03 +0100

    fix(server): modify jwt authentication

--- server/middleware/Auth.js
+++ server/middleware/Auth.js
@@ -1,10 +1,26 @@
-import expressJwt from 'express-jwt'
+// import expressJwt from 'express-jwt'
+
+import jwt from 'jsonwebtoken'
+
+// const requireSignIn = expressJwt({
+// 			secret:'kkdkkd'
+// 		})
 
 
-const requireSignIn = expressJwt({
-			secret:'kkdkkd'
+
+const requireSignIn = (req,res,next) => {
+	try{
+		const token = req.headers.Authorization.split(" ")[1]
+		const decoded = jwt.verify(token, 'kkdkkd')
+		req.decoded = decoded
+
+		next()
+
+	}catch(error) {
+		return res.status(401).json({
+			error: "Unauthorized Access, Please Login"
 		})
-
-
+	}
+}
 
 export default { requireSignIn }
